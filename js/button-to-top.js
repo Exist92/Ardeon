cb7279dd0aebdf332f5ee8f809668a746ae6b598
Andy <andypirs92@gmail.com>
2026-01-04 17:58:49 +0200

    preloader, btn to top, decor element, modal, animation

---
+++ js/button-to-top.js
@@ -0,0 +1,18 @@
+let mybutton = document.getElementById("myBtnTop");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
